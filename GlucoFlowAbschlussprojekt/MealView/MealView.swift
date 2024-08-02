02c0c607c1f7c88b922ae4df4b5d2859c0088c6b
BischofDennis <152178698+BischofDennis@users.noreply.github.com>
2024-08-02 15:23:24 +0200

    MealView hinzugefügt!^^

---
+++ GlucoFlowAbschlussprojekt/MealView/MealView.swift
@@ -0,0 +1,18 @@
+//
+//  MealView.swift
+//  GlucoFlowAbschlussprojekt
+//
+//  Created by Dennis Bischof on 02.08.24.
+//
+
+import SwiftUI
+
+struct MealView: View {
+    var body: some View {
+        Text("MealAPIView")
+    }
+}
+
+#Preview {
+    MealView()
+}
---
+++ GlucoFlowAbschlussprojekt/MealView/MealViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MealViewModel.swift
+//  GlucoFlowAbschlussprojekt
+//
+//  Created by Dennis Bischof on 02.08.24.
+//
+
+import Foundation
