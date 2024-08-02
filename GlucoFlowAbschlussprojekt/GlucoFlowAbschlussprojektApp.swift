ead9810c96579ef00b5617fc00d0d2d095c9891a
BischofDennis <152178698+BischofDennis@users.noreply.github.com>
2024-08-02 13:51:22 +0200

    GlucoFlow Abschlussprojekt iOS

---
+++ GlucoFlowAbschlussprojekt/GlucoFlowAbschlussprojektApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlucoFlowAbschlussprojektApp.swift
+//  GlucoFlowAbschlussprojekt
+//
+//  Created by Dennis Bischof on 02.08.24.
+//
+
+import SwiftUI
+
+@main
+struct GlucoFlowAbschlussprojektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
